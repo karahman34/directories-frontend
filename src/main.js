40cc0cb7d15c6acb54c6a4ab0f72da7f66a9512e
karahman <jakarahman007@gmail.com>
2021-02-28 12:28:00 +0700

    add @mdi/font

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 
+import '@mdi/font/css/materialdesignicons.min.css'
+
 Vue.config.productionTip = false
 
 new Vue({
