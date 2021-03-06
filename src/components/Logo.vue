444cf078c5a2a871b8ddf6353e52610c2fd4c3ad
karahman <jakarahman007@gmail.com>
2021-03-06 22:45:13 +0700

    use env app title

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="logo d-flex align-center">
     <v-icon class="mr-2 logo-icon">mdi-folder-text-outline</v-icon>
-    <span>Archiver</span>
+    <span>{{ appTitle }}</span>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    appTitle() {
+      return process.env.VUE_APP_TITLE
+    },
+  },
+}
+</script>
 
 <style lang="scss" scoped>
 .logo {
